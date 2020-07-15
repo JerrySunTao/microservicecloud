7d71beead0bae75fa3559e549d28e4bb5cc43a93
H.sun.tao <Pass@word1>
2020-07-15 20:09:54 +0800

    hystrix

--- microservicecloud-consumer-dept-feign/src/main/java/com/taosun/springcloud/DeptConsumer80_Feign.java
+++ microservicecloud-consumer-dept-feign/src/main/java/com/taosun/springcloud/DeptConsumer80_Feign.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients(basePackages = "com.taosun.springcloud")
+@EnableHystrix
 @ComponentScan("com.taosun.springcloud")
 public class DeptConsumer80_Feign {
 
